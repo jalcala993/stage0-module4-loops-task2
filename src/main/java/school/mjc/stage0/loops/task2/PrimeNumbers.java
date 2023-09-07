c442cd358081b8b5e0a1c7e7917801b9565cef41
jalcala993 <j.alcala.valeriano@gmail.com>
2023-09-07 16:17:05 +0000

    final solution

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,26 +2,26 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
-        int count = 2;
-        boolean dividend2;
-        boolean dividend3;
-        boolean dividend5;
-
-
-        while (count <= printToInclusive) {
-            dividend2 = count % 2 != 0;
-            dividend3 = count % 3 != 0;
-            dividend5 = count % 5 != 0;
-
-            if (count > 5 && dividend2 && dividend3 && dividend5) {
-                System.out.println(count);
-                ++count;
-            } else if (count == 2 || count == 3 || count == 5) {
-                System.out.println(count);
-                ++count;
-            } else {
-                ++count;
+        if(printToInclusive >= 2) {
+            System.out.println(2);
+        } 
+        int i = 3;
+        while(i < printToInclusive) {
+            int j = 2;
+            boolean natural = false; 
+            while(j < i){
+                if(i % j == 0){
+                    //not prime.
+                    natural = true;
+                    break;
+                } else {
+                    j++;
+                }
             }
+            if(!natural){
+                System.out.println(i);
+            }
+            i++;
         }
     }
 }

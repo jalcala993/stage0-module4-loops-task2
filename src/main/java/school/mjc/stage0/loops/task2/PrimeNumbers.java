f967fc8106cb5a63c87821d26b524d9ff7178230
jalcala993 <j.alcala.valeriano@gmail.com>
2023-09-07 00:16:32 +0000

    PrimeNumbers Solution

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        if(printToInclusive >= 2) {
+            System.out.println(2);
+        } 
+        int i = 3;
+        while(i < printToInclusive) {
+            int j = 2;
+            boolean natural = false; 
+            while(j < i){
+                if(i % j == 0){
+                    //not prime.
+                    natural = true;
+                    break;
+                } else {
+                    j++;
+                }
+            }
+            if(!natural){
+                System.out.println(i);
+            }
+            i++;
+        }
     }
 }

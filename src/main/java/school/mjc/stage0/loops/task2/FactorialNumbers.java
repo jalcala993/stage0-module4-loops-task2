7c6dabc8765849d2e5fb3e07525cbb610501fcbb
jalcala993 <j.alcala.valeriano@gmail.com>
2023-09-07 15:33:23 +0000

    Testing with all of Carlos' solutions

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int count = 0;
+        int factorial = 1;
+        while(count <= printToInclusive) {
+            System.out.println(factorial);
+            ++count;
+            factorial *= count;
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        int count = 0;
+        if (power<0){
+            System.out.println("too much power");
+            ++count;
+        }else {
+            while (count <= power) {
+                System.out.println(1 << count);
+                ++count;
+            }
+        }
 
     }
 }
